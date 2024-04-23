949ba0f7d4679ee71598a1562cd3bd8a19cda58f
Uyathandwa Nkosi <uyathandwasharon@gmail.com>
2024-04-23 13:20:30 +0200

    Add project files.

---
+++ ClassActivityWeek7/Program.cs
@@ -0,0 +1,36 @@
+﻿internal class Program
+{
+    public interface IPlayable
+    {
+        void Play();
+    }
+
+
+    class Piano : IPlayable
+    {
+
+
+        public void Play()
+        {
+            Console.WriteLine("PIANO IS NOW PLAYING");
+        }
+    }
+    class Guitor : IPlayable
+    {
+
+
+       public  void Play()
+        {
+
+            Console.WriteLine("Guitor IS NOW PLAYING");
+        }
+        public static void Main(string[] args)
+        {
+            Piano piano = new Piano();
+            Guitor guitor = new Guitor();
+            piano.Play();
+            guitor.Play();
+        }
+    }
+}
+
